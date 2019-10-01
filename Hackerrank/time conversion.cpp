cec55acefa7b79983b821c4a624b6f132112d7af
AvinashBonthu <47055975+AvinashBonthu@users.noreply.github.com>
2019-10-01 21:38:34 +0530

    Update time conversion.cpp

--- Hackerrank/time conversion.cpp
+++ Hackerrank/time conversion.cpp
@@ -1,110 +1,19 @@
-#include <bits/stdc++.h>
-
+ #include <math.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 using namespace std;
-
-/*
- * Complete the timeConversion function below.
- */
-string timeConversion(string s) 
-{
-    int l=s.length();
-    for(int i=0;i<=l; i++)
-    {
-        int h1=(int)s[0];
-        int h2=(int)s[1];
-        int hh=(h1*10+h2%10);
-        if(s[8]=='A')
-        {
-            if(s[0]=='1' && s[1]=='2')
-            {
-                s[0]='0';
-                s[1]='0';
-            }
-            s[8]=' ';
-            s[9]=' ';
-        }
-        else if(s[8]=='P')
-        {
-              
-                if(s[0]=='0' && s[1]=='1')
-                {
-                    s[0]='1';
-                    s[1]='3';
-                }
-            else if(s[0]=='0' && s[1]=='2')
-                {
-                    s[0]='1';
-                    s[1]='4';
-                }
-            else if(s[0]=='0' && s[1]=='3')
-                {
-                    s[0]='1';
-                    s[1]='5';
-                }
-             else if(s[0]=='0' && s[1]=='4')
-                {
-                    s[0]='1';
-                    s[1]='6';
-                }
-             else if(s[0]=='0' && s[1]=='5')
-                {
-                    s[0]='1';
-                    s[1]='7';
-                }
-             else if(s[0]=='0' && s[1]=='6')
-                {
-                    s[0]='1';
-                    s[1]='8';
-                }
-             else if(s[0]=='0' && s[1]=='7')
-                {
-                    s[0]='1';
-                    s[1]='9';
-                }
-             else if(s[0]=='0' && s[1]=='8')
-                {
-                    s[0]='2';
-                    s[1]='0';
-                }
-            else if(s[0]=='0' && s[1]=='9')
-            {
-                s[0]='2';
-                s[1]='1';
-            }
-             else if(s[0]=='1'&& s[1]=='0')
-                {
-                    s[0]='2';
-                    s[1]='2';
-                }
-             else if(s[0]=='1'&& s[1]=='1')
-                {
-                    s[0]='2';
-                    s[1]='3';
-                }
-             else 
-                {
-                    s[0]='1';
-                    s[1]='2';
-                } 
-                    s[8]=' ';
-                    s[9]=' ';
-            }
-        }
-    return s;
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string s;
-    getline(cin, s);
-
-    string result = timeConversion(s);
-
-    fout << result << "\n";
-
-    fout.close();
-
+int main(){
+    
+   
+    int hh, mm, ss ;
+    char t12[2];
+    scanf("%d:%d:%d%s", &hh, &mm, &ss, t12) ;
+    if (strcmp(t12,"PM")==0 && hh!=12) hh += 12 ;
+    if (strcmp(t12,"AM")==0 && hh==12) hh = 0 ;
+    printf("%02d:%02d:%02d", hh, mm, ss) ;
     return 0;
 }
