8cc5d8199dade6465186c23d395718ba8a897bc5
AvinashBonthu <47055975+AvinashBonthu@users.noreply.github.com>
2019-10-01 21:14:29 +0530

    Update Add_Two_Numbers.cpp

--- Leetcode/Add_Two_Numbers.cpp
+++ Leetcode/Add_Two_Numbers.cpp
@@ -1,27 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-      
-      ListNode preNode(-1), *p = &preNode;
-      int sum, carry = 0;
-      
-      while(l1 || l2 || carry) {
-          sum = (l1 ? l1->val:0) + (l2 ? l2->val:0) + carry;
-          p->next = new ListNode(sum%10);
-          carry = sum/10;
-          
-          p = p->next;
-          l1 = l1?l1->next:l1;
-          l2 = l2?l2->next:l2;
-      }
-      return preNode.next;
-  }  
-};
+class Solution:
+    def to_int(l: ListNode):
+        s = ''
+        while l != None:
+            s += str(l.val)
+            l = l.next
+        return int(s[::-1])
+    
+    def to_list(n: int):
+        s = str(n)[::-1]
+        head = prev = None
+        for ch in s:
+            node = ListNode(int(ch))
+            if prev is not None:
+                prev.next = node
+            prev = node
+            if head is None:
+                head = prev
+        return head
+    
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        a = Solution.to_int(l1)
+        b = Solution.to_int(l2)
+        return Solution.to_list(a + b)```
