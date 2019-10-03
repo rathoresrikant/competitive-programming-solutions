d2b6c5e046854eb36a61f7bf30720053c42f8ad6
shreyansh08 <31694560+shreyansh08@users.noreply.github.com>
2019-10-03 18:30:21 +0530

    Create DistinctCharactersQueries.cpp

---
+++ Codeforces/CF590D3/DistinctCharactersQueries.cpp
@@ -0,0 +1,252 @@
+#include <bits/stdc++.h>
+#include <time.h>
+#include <assert.h>
+
+
+using namespace std;
+
+
+
+#define For(i,x,n)  for(long long i=x;i<n;i++)
+#define rFor(i,x,n) for(long long i=x;i>=n;i--)
+#define itFor(it,st)   for(auto it=st.begin() ; it!=st.end() ; it++)
+#define MOD 1000000007
+#define PI 3.141592653589793
+#define INF 999999999999999999
+#define N 1000005
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sp " "
+#define cont continue
+#define brk break
+
+#define deb1(x)                cout<<#x<<": "<<x<<endl
+#define deb2(x, y)             cout<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define deb3(x, y, z)          cout<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define deb4(a, b, c, d)       cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define deb5(a, b, c, d, e)    cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define deb6(a, b, c, d, e, f) cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+using namespace std;
+
+
+
+void setprime(ll n=15000002);
+ll choose(ll n,ll k);
+void showArray(ll a[], ll n);
+ll gcd(ll a, ll b);
+ld len(ld x1, ld y1, ld x2, ld y2);
+ld gcd_ld(ld x, ld y);
+ll power(ll x, ll n);
+ll power1(ll x,ll n);
+
+int sstree[26][400010];
+int A[26][100001];
+
+void build(int idx, int node, int start, int end)
+{
+    if(start == end)
+    {
+        // Leaf node will have a single element
+        sstree[idx][node] = A[idx][start];
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        // Recurse on the left child
+        build(idx,2*node+1, start, mid);
+        // Recurse on the right child
+        build(idx,2*node+2, mid+1, end);
+        // Internal node will have the sum of both of its children
+        sstree[idx][node] = sstree[idx][2*node+1] + sstree[idx][2*node+2];
+    }
+}
+
+void update(int i,int node, int start, int end, int idx, int val)
+{
+    if(start == end)
+    {
+        // Leaf node
+        A[i][idx] += val;
+        sstree[i][node] += val;
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        if(start <= idx and idx <= mid)
+        {
+            // If idx is in the left child, recurse on the left child
+            update(i,2*node+1, start, mid, idx, val);
+        }
+        else
+        {
+            // if idx is in the right child, recurse on the right child
+            update(i,2*node+2, mid+1, end, idx, val);
+        }
+        // Internal node will have the sum of both of its children
+        sstree[i][node] = sstree[i][2*node+1] + sstree[i][2*node+2];
+    }
+}
+
+int query(int i,int node, int start, int end, int l, int r)
+{
+    if(r < start or end < l)
+    {
+        // range represented by a node is completely outside the given range
+        return 0;
+    }
+    if(l <= start and end <= r)
+    {
+        // range represented by a node is completely inside the given range
+        return sstree[i][node];
+    }
+    // range represented by a node is partially inside and partially outside the given range
+    int mid = (start + end) / 2;
+    int p1 = query(i,2*node+1, start, mid, l, r);
+    int p2 = query(i,2*node+2, mid+1, end, l, r);
+    return (p1 + p2);
+}
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+    cout<<fixed;
+    cout<<setprecision(3);
+    srand(time(NULL));
+
+    string s;
+    cin>>s;
+    int n = s.size();
+    For(i,0,s.size()){
+        ll idx = s[i]-'a';
+        A[idx][i] = 1;
+    }
+    For(i,0,26)
+        build(i,0,0,n-1);
+    ll q;
+    cin>>q;
+    For(zz,0,q){
+        ll t;
+        cin>>t;
+        if(t==1){
+            ll ps;
+            char c;
+            cin>>ps>>c;
+            ps--;
+            int idx = c-'a';
+            // (int i,int node, int start, int end, int idx, int val
+            if(s[ps] == c)
+                continue;
+            char old = s[ps];
+            s[ps] = c;
+            update(idx,0,0,n-1,ps,1);
+            // deb2(ps,query(idx,0,0,n-1,ps,ps));
+            idx = old-'a';
+            // deb1(A[idx][ps]);
+            update(idx,0,0,n-1,ps,-1);
+            // deb4(A[idx][ps],idx,ps,query(idx,0,0,n-1,0,n-1));
+        }
+        else{
+            ll l,r;
+            cin>>l>>r;
+            l--,r--;
+            ll cnt = 0;
+            For(i,0,26){
+                ll val = query(i,0,0,n-1,l,r);
+                
+                if(val != 0)
+                    cnt++;
+            }
+            cout<<cnt<<"\n";
+        }
+    }
+    
+    //cout<<"\nTime used = "<<static_cast<double>(clock())/(CLOCKS_PER_SEC)<<"s"<<endl;  // - for checking time used for a test case
+    return 0;
+}
+
+//s1.substr(1,3) - return 3 characters starting from position 1 as substring of string sz1. eg sz1="shreynumh" sz1.substr(2,5)="reyan"
+//num = atoi(s.c_str()); //convert string s to integer num
+//n.erase(i,std::string::npos); //erase i th position in string n , decreasing string size
+//s.find(b)!= std::string::npos - finds string b in string a - true if found
+//cout<<"Time used = "<<static_cast<double>(clock())/(CLOCKS_PER_SEC)<<"s"<<endl;  // - for checking time used for a test case
+
+
+void setprime(ll isprime[],ll n,ll cc=0)
+{
+    isprime[1]=1;
+    map<ll,ll> hm;
+    ll i;
+    for(i=2 ; i*i<=15000002 ; i++)
+        if(isprime[i]==0){
+            for(ll j=2*i ; j<=15000001 ; j+= i)
+                if(isprime[j]==0)
+                    isprime[j]=i;
+            isprime[i]=i;
+            hm[i] = cc;
+            cc++;
+        }
+    for( ; i<15000002 ; i++)
+        if(isprime[i]==0)
+            isprime[i]=i,hm[i]=cc,cc++;
+}
+
+ll choose(ll n,ll k)
+{
+    if(k==0)  return 1;
+    return (n* choose(n-1,k-1))/k;
+}
+
+void showArray(ll a[], ll n){
+    For(i,0,n){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+ll gcd(ll a, ll b)
+{
+    if(b==0)
+        return a;
+    return gcd(b,a%b);
+}
+
+ld len(ld x1, ld y1, ld x2, ld y2){
+    return sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+}
+
+ld gcd_ld(ld x, ld y) {
+    ld eps=0.0001;
+    while (fabs(x) > eps && fabs(y) > eps) {
+        if (x > y)
+                x -= floor(x / y) * y;
+        else
+                y -= floor(y / x) * x;
+    }
+    return x + y;
+}
+
+ll power(ll x,ll n)
+{
+    if(n==0)
+        return 1;
+    else if(n%2 == 0)        //n is even
+        return (power((x*x)%MOD,n/2))%MOD;
+    else                             //n is bal
+        return (x*power((x*x)%MOD,(n-1)/2))%MOD;
+}
+
+ll power1(ll x,ll n)
+{
+    ll num = 1;
+    For(i,0,n){
+        num = (num*x)%MOD;
+    }
+    return num;
+}                
