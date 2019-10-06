13f7290b46edecd8b8a0f02ec8af806168678dad
banrovegrie <52317522+banrovegrie@users.noreply.github.com>
2019-10-06 16:41:27 +0530

    Updated 1203B.cpp
    Updated the code and fixed a bug

--- Codeforces/1203B.cpp
+++ Codeforces/1203B.cpp
@@ -1,30 +1,32 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int a[(4*n)];
-		for(int i=0;i<4*n;i++)
-			cin>>a[i];
- 		int area=a[0]*a[4*n-1];
-		bool flag=true;
-		for(int i=0;i<n;i++) 
-		{
-			int x=2*i;
-			int y=4*n-(2*i)-1;
-			if(a[x]!=a[x+1]||a[y]!=a[y-1]||1ll*a[x]*a[y]!=area)
-				flag=false;
-		}
-		if(flag==true) 
-			cout<<"YES"<<endl;
-		else 
-			cout<<"NO"<<endl;
-	}
-	return
+#define ll long long int
+ 
+int main(){
+  
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+   	ll q,m,n,a[1005];
+   	cin>>q;
+   	while(q--)
+   	{	int flag=0;
+   		cin>>n;
+   		for(int i=0;i<4*n;i++)
+   			cin>>a[i];
+   		sort(a,a+4*n);
+   		m=a[0]*a[4*n-1];
+   		for(int l=0;l<4*n;l+=2)
+   		{
+   			if(m!=a[l]*a[4*n-l-1]||a[l]!=a[l+1])
+   				flag++;
+   		}
+   		if(flag==0)
+   			cout<<"YES"<<endl;
+   		else
+   			cout<<"NO"<<endl;
+   	}
+ 
+ 
+    return 0;
 }
