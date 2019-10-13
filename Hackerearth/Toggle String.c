77d69fd2559b8f6c5ed1c9c4886334edd4b6e0c8
ed-archer <56473062+ed-archer@users.noreply.github.com>
2019-10-13 15:16:54 +0530

    Add files via upload

---
+++ Hackerearth/Toggle String.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include <string.h>
+
+void main()
+
+{
+	char S[100];
+
+	scanf("%s",S);
+
+	for (int i=0; i<strlen(S); i++)
+
+	{
+
+		if(S[i]>=65 && S[i]<=90)
+
+		{
+
+			S[i]=S[i]+32;
+
+		}
+
+		else
+
+		{
+
+			S[i]=S[i]-32;
+
+		}
+
+	}
+
+	printf("%s\n",S);
+
+}
