72451c62dcb9df5a2b93dbc681c4cdabc89d4d4b
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-09-16 20:12:25 +0530

    Create Divisible-sum-of-pairs.cpp

---
+++ Hackerrank/Divisible-sum-of-pairs.cpp
@@ -0,0 +1,47 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    int count = 0;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[a_i];
+    }
+    
+    for(int i =0 ; i < n -1 ; i++){
+        for(int j=i+1 ; j < n ; j++){
+            if( (a[i]+a[j])%k ==0){
+                count++;
+            }
+        }
+    }
+    
+    cout << count;
+    return 0;
+}
