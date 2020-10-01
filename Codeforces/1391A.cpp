6723ccb7d8acba99fc873da1d29d4c15277be980
Anmol55555 <61626454+Anmol55555@users.noreply.github.com>
2020-10-01 21:43:22 +0530

    Solution of A. Suborrays 1391A.cpp

---
+++ Codeforces/1391A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t, n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        for(int i=1; i<=n; i++)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+}
