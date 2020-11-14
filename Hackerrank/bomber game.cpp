507d7288b59f50a23f5b0ac3ba4f9dc9a7f761c6
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-11-14 19:21:13 +0530

    Create bomber game.cpp

---
+++ Hackerrank/bomber game.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the bomberMan function below.
+vector<string> bomberMan(int n, vector<string> grid) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string rcn_temp;
+    getline(cin, rcn_temp);
+
+    vector<string> rcn = split_string(rcn_temp);
+
+    int r = stoi(rcn[0]);
+
+    int c = stoi(rcn[1]);
+
+    int n = stoi(rcn[2]);
+
+    vector<string> grid(r);
+
+    for (int i = 0; i < r; i++) {
+        string grid_item;
+        getline(cin, grid_item);
+
+        grid[i] = grid_item;
+    }
+
+    vector<string> result = bomberMan(n, grid);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+// This program is contributed by MOhammad Tanzil Idrisi
