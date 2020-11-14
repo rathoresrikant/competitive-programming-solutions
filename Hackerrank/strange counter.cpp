35dc23554fd97176d1db8c4fbcb9feee432060e8
Mohammad Tanzil Idrisi <44720341+tanzil786@users.noreply.github.com>
2020-11-14 19:13:36 +0530

    Create strange counter.cpp

---
+++ Hackerrank/strange counter.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the strangeCounter function below.
+long strangeCounter(long t) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    long t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+// This program is written by  mohammad Tanzil Idrisi
